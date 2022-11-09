5397245c19c6009b2618a79ed83d4350c0911ddc
pranjal <agrawalpranjal15@gmail.com>
2022-11-09 15:14:11 +0530

    Add styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
 import propTypes from 'prop-types'
 
-function Header({ text }){
+function Header({ text, bgColor, textColor  }){
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
+    
     return(
-        <header>
+        <header style={headerStyles}>
+            <div className='container'>
             <h1>{text}</h1>
+            </div>
         </header>
     )
 }
 
 Header.defaultProps = {
-    text: "Defualt tezt"
+    text: "Defualt text",
+    bgColor: 'rgba(0,0,0,0.7)',
+    textColor: '#ff6a95',
 }
 
 Header.propTypes = {
-    text : propTypes.string
+    text : propTypes.string,
+    bgColor: propTypes.string,
+    textColor: propTypes.string
 }
 export default Header
