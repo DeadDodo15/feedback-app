561534766ef66ac5346d13d9fa5ad4f30c0748fe
pranjal <agrawalpranjal15@gmail.com>
2022-11-11 15:34:52 +0530

    event and prop drilling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import FeedbackData from "./data/FeedbackData"
 function App(){
     const [Feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(Feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return <>
     <Header text="Feedback UI" />
     <div className="container">
-    <FeedbackList feedback={Feedback}/>
+    <FeedbackList feedback={Feedback} handleDelete={deleteFeedback}/>
     </div>
     </>
 }
